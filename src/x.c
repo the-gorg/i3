28775b4d566b4b8db0cfb025a57399cff3263182
gorg <gorgmakesthings@gmail.com>
2021-03-23 18:34:13 +0100

    Added styled border for tabs/stacks.

--- src/x.c
+++ src/x.c
@@ -360,21 +360,21 @@ static void x_draw_title_border(Con *con, struct deco_render_params *p) {
 
     Rect *dr = &(con->deco_rect);
 
-    /* Left */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
-                        dr->x, dr->y, 1, dr->height);
-
-    /* Right */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
-                        dr->x + dr->width - 1, dr->y, 1, dr->height);
+//    /* Left */
+//    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+//                        dr->x, dr->y, 1, dr->height);
+//
+//    /* Right */
+//    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+//                        dr->x + dr->width - 1, dr->y, 1, dr->height);
 
     /* Top */
     draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
-                        dr->x, dr->y, dr->width, 1);
+                        dr->x, dr->y, dr->width, 3);
 
-    /* Bottom */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
-                        dr->x, dr->y + dr->height - 1, dr->width, 1);
+//    /* Bottom */
+//    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+//                        dr->x, dr->y + dr->height - 1, dr->width, 1);
 }
 
 static void x_draw_decoration_after_title(Con *con, struct deco_render_params *p) {
@@ -700,7 +700,7 @@ void x_draw_decoration(Con *con) {
     draw_util_text(title, &(parent->frame_buffer),
                    p->color->text, p->color->background,
                    con->deco_rect.x + title_offset_x,
-                   con->deco_rect.y + text_offset_y,
+                   con->deco_rect.y + text_offset_y + 2,
                    deco_width - mark_width - 2 * title_padding);
 
     if (win == NULL || con->title_format != NULL) {
